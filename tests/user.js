0a43932b575b35b6ab9fc14735f5805341daa736
Željko Filipin <zeljko.filipin@gmail.com>
2016-12-12 17:32:23 +0100

    User should be able to change preferences

--- tests/user.js
+++ tests/user.js
@@ -12,6 +12,17 @@ module.exports = {
 		.url( client.launch_url + 'Special:UserLogin' ) // jscs:ignore requireCamelCaseOrUpperCaseIdentifiers
 		.assert.elementPresent( '#wpLoginAttempt' )
 		.end();
+	},
+
+	'User should be able to chage preferences': function ( client ) {
+		client
+		.url( client.launch_url + 'Special:UserLogin' ) // jscs:ignore requireCamelCaseOrUpperCaseIdentifiers
+		.setValue( '#wpName1', 'Admin' )
+		.setValue( '#wpPassword1', 'vagrant' )
+		.click( '#wpLoginAttempt' )
+		.url( client.launch_url + 'Special:Preferences' ) // jscs:ignore requireCamelCaseOrUpperCaseIdentifiers
+		.assert.elementPresent( '#prefcontrol' )
+		.end();
 	}
 
 };
