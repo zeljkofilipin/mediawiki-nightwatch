34015a08bc304e842eab1855ccaef1e0ba5ed244
Željko Filipin <zeljko.filipin@gmail.com>
2016-12-12 17:24:54 +0100

    User should be able to log in

--- tests/user.js
+++ tests/user.js
@@ -5,6 +5,13 @@ module.exports = {
 		.url( client.launch_url + 'Special:CreateAccount' ) // jscs:ignore requireCamelCaseOrUpperCaseIdentifiers
 		.assert.elementPresent( '#wpCreateaccount' )
 		.end();
+	},
+
+	'User should be able to log in': function ( client ) {
+		client
+		.url( client.launch_url + 'Special:UserLogin' ) // jscs:ignore requireCamelCaseOrUpperCaseIdentifiers
+		.assert.elementPresent( '#wpLoginAttempt' )
+		.end();
 	}
 
 };
