217b40edf387a0de79a2d0c113576a38dd808d38
Željko Filipin <zeljko.filipin@gmail.com>
2016-12-12 17:06:43 +0100

    Page should be edited

--- tests/page.js
+++ tests/page.js
@@ -1,8 +1,17 @@
 module.exports = {
+
 	'Page should be created': function ( client ) {
 		client
 		.url( client.launch_url + 'Does_not_exist' ) // jscs:ignore requireCamelCaseOrUpperCaseIdentifiers
 		.assert.elementPresent( 'li#ca-edit a' )
 		.end();
+	},
+
+	'Page should be edited': function ( client ) {
+		client
+		.url( client.launch_url + 'Main_Page' ) // jscs:ignore requireCamelCaseOrUpperCaseIdentifiers
+		.assert.elementPresent( 'li#ca-edit a' )
+		.end();
 	}
+
 };
